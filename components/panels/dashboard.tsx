451b52788f13f6ac7f190ac51be583d4722654dc
Hornet <hornet.dev.0802@gmail.com>
2023-04-28 23:34:06 -0400

    # dashboard first

--- components/panels/dashboard.tsx
+++ components/panels/dashboard.tsx
@@ -1,24 +1,69 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Head from "next/head";
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
 
 import Title from "./Title";
+
+import Modal from '@mui/material/Modal';
 
 const Dashboard = () => {
 
     const [totalNumber, setTotalNumber] = useState<number>(0);
     const [avgNumber, setAvgNumber] = useState<number>(0);
 
+    const [modalState, setModalState] = useState<boolean>(false);
+
     const [logList, setLogList] = useState([
-        { usermsg: "Hello", botmsg: "Hello, What can I help you?", msgtime: "Just now"},
-        { usermsg: "Hello, What's your name?", botmsg: "The technologies used in the given context include Sidekiq, Redis, Caching, Elasticsearch, Pusher, Memcached, Ruby on Rails, TailwindCSS, RSpec, Capybara, Pheonix Liveview, Node.js, React, Vue.js, Redux, API, GraphQL, PostgreSQL, MySQL, Redis, MongoDB, Kibana, SQS, ECS, Beanstalk, and CI/CD.", msgtime: "5 hours age"},
-        { usermsg: "who are you?", botmsg: "TMy name is AI Assistant. I am here to assist you with any questions you have regarding the given information.", msgtime: "8 hours age"},
+        { conversation_id: "1", usermsg: "Hello", botmsg: "Hello, What can I help you?", msgtime: "Just now"},
+        { conversation_id: "2", usermsg: "Hello, What's your name?", botmsg: "The technologies used in the given context include Sidekiq, Redis, Caching, Elasticsearch, Pusher, Memcached, Ruby on Rails, TailwindCSS, RSpec, Capybara, Pheonix Liveview, Node.js, React, Vue.js, Redux, API, GraphQL, PostgreSQL, MySQL, Redis, MongoDB, Kibana, SQS, ECS, Beanstalk, and CI/CD.", msgtime: "5 hours age"},
+        { conversation_id: "3", usermsg: "who are you?", botmsg: "My name is AI Assistant. I am here to assist you with any questions you have regarding the given information.", msgtime: "8 hours age"},
     ])
+
+    const handleClose = () => setModalState(false);
+
+    const [messages, setMessages] = useState([
+        { content: "Hi! What can I answer for you today?", role: "assistant" },
+      ]);
+
+    const log_select = async (conversation_id: any) => {
+        localStorage.setItem("conversation_id", conversation_id);
+        console.log(conversation_id);
+        setModalState(true);
+        console.log(modalState);
+    };
 
     return (
         <Fragment>
             <Head>
                 <title>Setting</title>
             </Head>
+            <Modal open={modalState} onClose={handleClose} >
+                <div id="chatlog-container">
+                    <div className="chatlog-widget">
+                        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                            <ArrowPathIcon className="reload" />
+                        </div>
+                        <hr />
+                        <div className="widget">
+                            {/* Render the messages */}
+                            <div className="chat-widget">
+                                <div className="chat-content">
+                                    {messages.map((message, index) => (
+                                    <div
+                                        key={index}
+                                        className={`message ${
+                                        message.role === "user" ? "user" : "bot"
+                                        }`}
+                                    >
+                                        {message.content}
+                                    </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
             <section id="dashboardPage">
                 <div className="widget">
                     <Title title="Dashboard" />
@@ -42,7 +87,7 @@ const Dashboard = () => {
                         <span className="title">Conversations</span>
                         <ul className="msglog-list">
                             {logList.map((log, index) => (
-                                <li key={index} className="msglog-form">
+                                <li key={index} className="msglog-form" onClick={() => log_select(log["conversation_id"])}>
                                     <div className="msg-1">
                                         <span className="user-msg">{"User: " + log.usermsg}</span>
                                         <span className="msg-time">{log.msgtime}</span>
